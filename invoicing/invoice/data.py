e00d2a2e8506ae6d27dde7eb4986a0a6161bded2
unknown <krupakar123k@gmail.com>
2023-07-15 11:46:46 +0530

    first commit

---
+++ invoicing/invoice/data.py
@@ -0,0 +1,23 @@
+invoicesData=[
+    {
+    "invoice_id":1,
+    "client_name":"krupakar",
+    "date":"2023-05-30",
+    "items":[
+        {
+        "desc":"product 1",
+        "rate":100,
+        "quantity":2
+    }
+    ],
+}
+]
+userData=[
+    {
+    "user_id":1,
+    "name":"krupakar",
+    "email":"krupakar123k@gmail.com",
+    "password":"123456",
+}
+]
+
